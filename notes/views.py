559ddb3e95c8e47fe80d92c0fae7c9e371d06b50
chistev <stephenowabie@gmail.com>
2024-10-03 22:16:13 +0100

    added footer, modified notes to use created at date if not updated yet

--- notes/views.py
+++ notes/views.py
@@ -1,23 +1,34 @@
+from datetime import datetime
+
+from django.db.models import F
+from django.db.models.functions import Coalesce
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Note
 
 
 def notes_view(request):
-    notes = Note.objects.all().order_by('-updated_at')
-    return render(request, 'notes/index.html', {'notes': notes})
+    # Annotate to use updated_at if available, otherwise use created_at
+    notes = Note.objects.annotate(
+        effective_date=Coalesce('updated_at', F('created_at'))
+    ).order_by('-effective_date')
+
+    current_year = datetime.now().year
+    return render(request, 'notes/index.html', {'notes': notes, 'current_year': current_year})
 
 
 def note_detail_view(request, title):
     note = get_object_or_404(Note, title=title)
     query = request.GET.get('query', '')
-    return render(request, 'notes/detail.html', {'note': note, 'query': query})
+    current_year = datetime.now().year
+    return render(request, 'notes/detail.html', {'note': note, 'query': query, 'current_year': current_year})
 
 
 def search_notes(request):
     query = request.GET.get('query', '')
     notes = Note.objects.filter(content__icontains=query)
-    return render(request, 'notes/note_list.html', {'notes': notes})
+    current_year = datetime.now().year
+    return render(request, 'notes/note_list.html', {'notes': notes, 'current_year': current_year})
 
 
 def delete_note(request, title):
